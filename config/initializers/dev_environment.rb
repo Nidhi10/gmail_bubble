eebc9c45d65ee34e5d98a754c01b92c1d982bac5
Nidhi10 <nidhi105@gmail.com>
2015-05-10 11:22:41 +0530

    Update dev_environment.rb

--- config/initializers/dev_environment.rb
+++ config/initializers/dev_environment.rb
@@ -1,4 +1,4 @@
 unless Rails.env.production?
-  ENV['CLIENT_ID'] = '716334124532-fkodcs7belej4b9jcc6692316slqfv7s.apps.googleusercontent.com'
-  ENV['CLIENT_SECRET'] = 't39kcX7o7vKk7-F51cVIkg3N'
+  ENV['CLIENT_ID'] = ''
+  ENV['CLIENT_SECRET'] = ''
 end
